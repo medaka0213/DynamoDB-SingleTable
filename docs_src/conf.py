bee021e1a86401a35800796a41356683163b4462
medaka0228 <anywherefish@gmail.com>
2022-09-30 21:35:46 +0900

    ドキュメントのテーマが適用されない

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -32,5 +32,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+#追加
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+]
