f95915489ca1ded10952c93740389820c61bc947
medaka0228 <anywherefish@gmail.com>
2022-10-02 19:13:42 +0900

    ドキュメント修正

--- release-prd.ps1
+++ release-prd.ps1
@@ -1,6 +1,7 @@
 pandoc -f markdown -t rst -o readme.rst readme.md
 python -m readme_renderer readme.rst -o readme.html
 
+Copy-Item -Path readme.md -Destination docs_src/readme.md  -Force
 sphinx-apidoc -f -o ./docs_src ./ddb_single
 sphinx-build ./docs_src ./docs
 
